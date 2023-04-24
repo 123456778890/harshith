bf815c44ce7140821388cc16683d78a74564dd73
123456778890 <129049004+123456778890@users.noreply.github.com>
2023-04-24 17:33:58 +0700

    Update one.java

--- one.java
+++ one.java
@@ -1,5 +1,5 @@
 public class one{
 	public static void main (String [ ] args){
-		System.out.println("hello wold chenge1 chenge2");
+		System.out.println("hello wold chenge1 chenge2 harshitha");
 	}
 }
